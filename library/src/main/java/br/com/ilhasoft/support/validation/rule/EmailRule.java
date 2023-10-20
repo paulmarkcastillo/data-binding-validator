44512e6216f86ddbda47a795c6765a3997c639a8
Keenn Howard Caw <kcaw@isbx.com>
2023-10-20 16:49:04 +0800

    test

--- library/src/main/java/br/com/ilhasoft/support/validation/rule/EmailRule.java
+++ library/src/main/java/br/com/ilhasoft/support/validation/rule/EmailRule.java
@@ -22,17 +22,17 @@ import br.com.ilhasoft.support.validation.util.EditTextHandler;
 
 public class EmailRule extends Rule<TextView, String> {
 
-    private static final int USERNAME_LENGTH =  64;
+    private static final int USERNAME_LENGTH = 64;
 
     public EmailRule(TextView view, String value, String errorMessage) {
-        super(view, value,  errorMessage);
+        super(view, value, errorMessage);
     }
 
     @Override
     public boolean isValid(TextView view) {
         String emailValue = view.getText().toString();
         String username = emailValue.split("@")[0];
-        return username.length() <= USERNAME_LENGTH && emailValue.matches(value);
+        return false;
     }
 
     @Override
